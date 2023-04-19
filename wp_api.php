3f50a6547eae1d336e3bcd1606d861ee2afbce5a
sidharthrajwal <131239031+sidharthrajwal@users.noreply.github.com>
2023-04-19 12:51:35 +0530

    Create wp_api.php

---
+++ wp_api.php
@@ -0,0 +1,28 @@
+<style type="text/css">
+ input.title-input
+{
+
+    line-height: 10px;
+}
+
+.admin-quick-add {
+    background-color: #DDD;
+    padding: 15px;
+    margin-top: 100px;
+}
+
+.admin-quick-add input,
+.admin-quick-add textarea {
+    width: 100%;
+    border: none;
+    padding: 10px;
+    margin: 0 0 10px 0;
+    box-sizing: border-box;
+}</style>
+<div class="admin-quick-add">
+  <h3>Add New Post</h3>
+  <input type="text" name="title" placeholder="Title">
+  <input name="content" placeholder="Content"></input>
+  <button id="quick-add-button">Create Post</button>
+  
+</div>
